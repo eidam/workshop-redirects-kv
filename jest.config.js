35cee4341cdb417d28b62c3fd3ca2bc426b06381
Adam Janiš <adam.janis@gmail.com>
2022-04-10 00:00:50 +0100

    refactor tests

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,5 @@
 export default {
     testEnvironment: "miniflare",
-    // Configuration is automatically loaded from `.env`, `package.json` and
-    // `wrangler.toml` files by default, but you can pass any additional Miniflare
-    // API options here:
-    testEnvironmentOptions: {
-        modules: true,
-        bindings: { KEY: "value" },
-        kvNamespaces: ["TEST_NAMESPACE"]
-    },
     transform: {
         "^.+\\.c?(t|j)sx?$": [
             "esbuild-jest",
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,43 @@
 import worker from "../src/index";
 
-test("responds with hello world", async () => {
-  const req = new Request("http://localhost/");
-  const res = await worker.fetch(req);
-  expect(await res.text()).toBe("Hello World!");
-});
+const redirects = new Map();
+redirects.set('/workers', "https://workers.cloudflare.com/");
+redirects.set('/pages', "https://pages.cloudflare.com/");
+redirects.set('/google', "https://google.com/");
+
+// @ts-ignore
+const { KV_REDIRECTS } = getMiniflareBindings()
+
+describe('Wrangler configuration', () => {
+  test("KV_REDIRECTS KV namespace binding exists", async () => {
+    expect(KV_REDIRECTS).toBeDefined()
+  })
+})
+
+describe('Default behaviour', () => {
+  test("homepage responds with hello redirects", async () => {
+    const req = new Request("http://localhost/");
+    const res = await worker.fetch(req, {KV_REDIRECTS});
+    expect(await res.text()).toBe("Hello Redirects!");
+  });
+})
+
+describe('KV Redirects', () => {
+  const uuid = crypto.randomUUID()
+  const randomPath = "/random"
+  const randomLocation = `https://${uuid}.example.com/`
+
+  test(`/nonexistent returns 404`, async () => {
+    const req = new Request("http://localhost/nonexistent");
+    const res = await worker.fetch(req, {KV_REDIRECTS});
+    expect(res.status).toBe(404);
+  })
+
+  test(`${randomPath} redirects to ${randomLocation}`, async () => {
+    await KV_REDIRECTS.put(randomPath, randomLocation);
+    const req = new Request(`http://localhost${randomPath}`);
+    const res = await worker.fetch(req, {KV_REDIRECTS});
+    expect(res.status).toBe(302);
+    expect(res.headers.get("Location")).toBe(randomLocation);
+  })
+})
