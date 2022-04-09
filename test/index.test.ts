2255a4031478a7131288792e4f1fd193fa120d5f
Adam Janiš <adam.janis@gmail.com>
2022-04-10 00:11:08 +0100

    remove static test redirects

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,5 @@
 import worker from "../src/index";
 
-const redirects = new Map();
-redirects.set('/workers', "https://workers.cloudflare.com/");
-redirects.set('/pages', "https://pages.cloudflare.com/");
-redirects.set('/google', "https://google.com/");
-
 // @ts-ignore
 const { KV_REDIRECTS } = getMiniflareBindings()
 
@@ -17,6 +12,7 @@ describe('Wrangler configuration', () => {
 describe('Default behaviour', () => {
   test("homepage responds with hello redirects", async () => {
     const req = new Request("http://localhost/");
+    // @ts-ignore
     const res = await worker.fetch(req, {KV_REDIRECTS});
     expect(await res.text()).toBe("Hello Redirects!");
   });
@@ -29,6 +25,7 @@ describe('KV Redirects', () => {
 
   test(`/nonexistent returns 404`, async () => {
     const req = new Request("http://localhost/nonexistent");
+    // @ts-ignore
     const res = await worker.fetch(req, {KV_REDIRECTS});
     expect(res.status).toBe(404);
   })
@@ -36,6 +33,7 @@ describe('KV Redirects', () => {
   test(`${randomPath} redirects to ${randomLocation}`, async () => {
     await KV_REDIRECTS.put(randomPath, randomLocation);
     const req = new Request(`http://localhost${randomPath}`);
+    // @ts-ignore
     const res = await worker.fetch(req, {KV_REDIRECTS});
     expect(res.status).toBe(302);
     expect(res.headers.get("Location")).toBe(randomLocation);
